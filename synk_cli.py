29e0a5e410b3b82febfc21da9c4de49534d60340
fa11en <fa11en@localhost.localdomain>
2017-04-09 19:18:10 -0400

    test

--- synk_cli.py
+++ synk_cli.py
@@ -70,7 +70,6 @@ def main():
     if not os.path.isfile("~/.config/synk.conf"):
         print("Please run `./synk_cli.py -s` first.")
         os.quit()
-        break
     while True:
         try:
             if detect_changes:
