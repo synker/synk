df9af5b4dea3ce829aa464fc7a3cd7203387eff6
K4YT3X <k4t@protonmail.com>
2017-04-09 15:27:12 -0400

    autocommit

--- synk.py
+++ synk.py
@@ -35,7 +35,7 @@ def sendFile(conn, filename, orgfname, last):
 def syncDir():
     sock0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     avalon.info('Trying to connect to ' + RHOST + 'on port ' + str(RPORT))
-    for file in os.listdi
+    for file in os.listdir()
     sock0.connect((RHOST, RPORT))
 
 
