227fcd1afbe8f7f6d25db0f81f143e96290a01fd
fa11en <fa11en@localhost.localdomain>
2017-04-10 08:58:58 -0400

    autocommit

--- synk_cli.py
+++ synk_cli.py
@@ -81,7 +81,7 @@ def main():
         print(detect_changes())
         try:
             if detect_changes():
-                os.system("git add -f . && git commit -am 'autocommit' && git stash create 'autostash'")
+                os.system("git add -f . && git commit -am 'autocommit' && git stash save")
                 get_changes()
                 upload_changes()
                 continue
